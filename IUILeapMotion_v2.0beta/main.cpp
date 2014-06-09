a830930785a961e08d3dad1274abdd2ea225eb2d
Carolyn <carolyn82416@gmail.com>
2014-06-09 18:11:14 +0800

    Initial Commit

---
+++ IUILeapMotion_v2.0beta/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  IUILeapMotion_v2.0beta
+//
+//  Created by Carolyn on 2014/6/9.
+//  Copyright (c) 2014年 Carolyn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
