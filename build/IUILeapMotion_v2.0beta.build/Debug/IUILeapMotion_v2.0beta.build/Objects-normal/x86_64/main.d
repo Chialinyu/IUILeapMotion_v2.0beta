47a8aaa1521617e8f43208c0ae42799dc96d8871
Carolyn <carolyn82416@gmail.com>
2014-06-10 06:01:55 +0800

    first

---
+++ build/IUILeapMotion_v2.0beta.build/Debug/IUILeapMotion_v2.0beta.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/carolyn/Desktop/IUILeapMotion_v2.0beta/IUILeapMotion_v2.0beta/main.cpp \
+  /Users/carolyn/Desktop/IUILeapMotion_v2.0beta/Leap.h \
+  /Users/carolyn/Desktop/IUILeapMotion_v2.0beta/LeapMath.h
